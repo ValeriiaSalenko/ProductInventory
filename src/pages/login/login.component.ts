01f3058500d6036a3aacaf42cfc0603a89c6d960
MotleyWildside <motleyWildside@gmail.com>
2018-05-03 14:37:40 +0300

    validation

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -17,6 +17,11 @@ export class LoginPage {
   @ViewChild('email') email;
   @ViewChild('password') password;
 
+  errorEmail1: boolean = false;
+  errorEmail2: boolean = false;
+  errorPass1: boolean = false;
+  errorPass2: boolean = false;
+
   constructor(public navCtrl: NavController) {
 
   }
@@ -24,6 +29,44 @@ export class LoginPage {
     this.navCtrl.push(RegistrationPage, {
       item: item
     });
+  }
+
+  signIn() {
+    this.errorEmail1 = false;
+    this.errorEmail2 = false;
+    this.errorPass1 = false;
+    this.errorPass2 = false;
+    if (this.email.value !== '') {
+      if (this.password.value !== '') {
+        let users = JSON.parse(localStorage.getItem('itemsArray')) || [];
+
+        for (let i = 0; i < users.length; i++) {
+          if (users[i].email == this.email.value) {
+            var userF = true;
+            if (users[i].password == this.password.value) {
+              alert("Login confimed!");
+              return;
+            } else {
+              this.errorPass2 = true;
+
+              // alert("Wrong password!!!");
+              return;
+            }
+          }
+        }
+        if(!userF) {
+          this.errorEmail1 = true;
+          //alert("User not found!");
+        }
+      } else {
+        this.errorPass1 = true;
+        // alert("Enter password!");
+      }
+    } else {
+      console.log(this.email);
+      this.errorEmail2 = true;
+      //alert("Enter e-mail!");
+    }
   }
 
 
--- src/pages/registration/registration.component.ts
+++ src/pages/registration/registration.component.ts
@@ -19,6 +19,13 @@ export class RegistrationPage {
   @ViewChild('password') password;
   @ViewChild('repPass') repPass;
 
+  errorEmail1: boolean = false;
+  errorEmail2: boolean = false;
+  errorEmail3: boolean = false;
+  errorPass1: boolean = false;
+  errorPass2: boolean = false;
+  regEx = /^\w+@\w+\.\w{2,4}$/i;
+
   constructor(public navCtrl: NavController) {
 
   }
@@ -29,38 +36,53 @@ export class RegistrationPage {
     });
   }
   regMe(){
+    this.errorEmail1 = false;
+    this.errorEmail2 = false;
+    this.errorEmail3 = false;
+    this.errorPass1 = false;
+    this.errorPass2 = false;
     //console.log(this.email.value);
-    if(!!this.email.value) {
-      if(this.password.value.length >= 6 ) {
-        if (this.password.value == this.repPass.value && !!this.password.value && !!this.repPass.value) {
+    if (this.regEx.test(this.email.value)) {
+      if (!!this.email.value) {
+        if (this.password.value.length >= 6) {
+          // console.log(this.password.value, this.repPass.value);
+          if (this.password.value == this.repPass.value && !!this.password.value && !!this.repPass.value) {
 
 
-          let oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
-          for (let i = 0; i < oldItems.length; i++) {
-            if (oldItems[i].email == this.email.value) {
-              alert('This user already register!');
-              return;
+            let oldItems = JSON.parse(localStorage.getItem('itemsArray')) || [];
+            for (let i = 0; i < oldItems.length; i++) {
+              if (oldItems[i].email == this.email.value) {
+                this.errorEmail1 = true;
+                //alert('This user already register!');
+                return;
+              }
+
             }
+            var newItem = {
+              'email': this.email.value,
+              'password': this.password.value
+            };
 
+            oldItems.push(newItem);
+
+            localStorage.setItem('itemsArray', JSON.stringify(oldItems));
+
+            alert("Thank you for registration!");
+          } else {
+            this.errorPass1 = true;
+            //alert("Passwords do not match!");
           }
-          var newItem = {
-            'email': this.email.value,
-            'password': this.password.value
-          };
-
-          oldItems.push(newItem);
-
-          localStorage.setItem('itemsArray', JSON.stringify(oldItems));
-
-          alert("Thank you for registration!");
         } else {
-          alert("Passwords do not match!");
+          this.errorPass2 = true;
+          //alert("Password must be at least 6 characters long");
         }
       } else {
-        alert("Password must be at least 6 characters long");
+        this.errorEmail2 = true;
+        //alert("Enter the e-mail, please");
       }
     } else {
-      alert("Enter the e-mail, please");
+      this.errorEmail3 = true;
+      //alert('ВВЕДИ МЫЛО, ДУРАК')
     }
 
     // var mass = JSON.parse(localStorage.getItem('itemsArray')) || [];
